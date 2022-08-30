9464503cb7b41b6a03f1951136fbad81250c4b09
jeshc <hcjesus@gmail.com>
2022-08-30 11:21:53 -0500

    See sube la tarea 5

---
+++ tareas/tarea5/Tarea5_Conjuntos/src/tarea5_conjuntos/Tarea5_Conjuntos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea5_conjuntos;
+
+import ico.fes.edd.Conjunto;
+
+/**
+ *
+ * @author Cabrera
+ */
+public class Tarea5_Conjuntos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+        Conjunto set = new Conjunto("Esta es una preuba");
+        System.out.println(set);
+    }
+    
+}
